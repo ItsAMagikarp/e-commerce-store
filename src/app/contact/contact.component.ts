baad67f61192c4847d79840b1e1f68824af116fc
ItsAMagikarp <tbrian8726@gmail.com>
2024-08-25 00:41:59 -0400

    created contact page with angular materials

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatFormField, MatButtonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
