c701314adf24da978879e93bb42a3c22eb45b71e
ItsAMagikarp <tbrian8726@gmail.com>
2024-08-25 00:51:42 -0400

    Removed code that updates category because not needed anymore

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,4 +1,4 @@
-/* import { CategoriesService } from './../service/categories.service';
+import { CategoriesService } from './../service/categories.service';
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, RouterModule} from '@angular/router';
@@ -21,48 +21,7 @@ export class CategoriesComponent implements OnInit{
 
   ngOnInit() {
     const categoryName = String(this.route.snapshot.paramMap.get('categoryName'));
-    console.log('Category Name:', categoryName); 
-    const categoryDetails = this.CategoriesService.categoryList.find(c => c.categoryName === categoryName); 
     this.category = categoryName
   }
-} */
+}
 
-  import { CategoriesService } from './../service/categories.service';
-  import { CommonModule } from '@angular/common';
-  import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
-  import { ActivatedRoute, RouterModule} from '@angular/router';
-  import { ProductsComponent } from '../products/products.component';
-  
-  @Component({
-    selector: 'app-categories',
-    standalone: true,
-    imports: [CommonModule, RouterModule, ProductsComponent],
-    templateUrl: './categories.component.html',
-    styleUrl: './categories.component.css'
-  })
-  export class CategoriesComponent implements OnInit, OnChanges {
-    @Input() category!: any;
-  
-    constructor(
-      private route: ActivatedRoute,
-      private CategoriesService: CategoriesService
-    ) {}
-  
-    ngOnInit() {
-      this.updateCategory();
-      this.route.paramMap.subscribe(() => this.updateCategory()); // Listen for changes
-    }
-  
-    ngOnChanges(changes: SimpleChanges) {
-      if (changes['category']) {
-        this.updateCategory();
-      }
-    }
-  
-    private updateCategory() {
-      const categoryName = String(this.route.snapshot.paramMap.get('categoryName'));
-      console.log('Category Name:', categoryName); 
-      const categoryDetails = this.CategoriesService.categoryList.find(c => c.categoryName === categoryName); 
-      this.category = categoryDetails?.categoryName;
-    }
-  }
